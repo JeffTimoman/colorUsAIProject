ad73d7e8555ef54d21d6c83e30e810fb1fa4e779
Jefferson Timotius Mansur <122242388+JeffTimoman@users.noreply.github.com>
2023-09-12 20:28:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from webdata import app
 
 if __name__ == '__main__':
     with app.app_context():
-        #app.run(debug=True)
-	    app.run(debug=False, host='10.68.103.190', port=80)
+        app.run(debug=True)
+	#app.run(debug=False, host='10.68.103.190', port=80)
