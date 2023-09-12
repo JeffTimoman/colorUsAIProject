b0fe2e3c2e3b4536f643e8aa4eb389f2b9184dc0
JeffTimoman <ppti.jeffersontimotius@gmail.com>
2023-09-12 19:56:58 +0700

    Bug Fix

--- webdata/config.py
+++ webdata/config.py
@@ -9,8 +9,7 @@ class Config:
         self.DB_PASSWORD = ''
         self.DB_PORT = '3306'
         self.UPLOAD_FOLDER = 'webdata/static/uploads'
-        self.API_KEY = 'isi sendiri ya'
-        
+        self.API_KEY = 'isi sendiri yak'
         self.SECRET_KEY = 'This is a secret key'
         
         self.DB_URL = f"{self.DB_PLATFORM}://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_SERVER}:{self.DB_PORT}/{self.DB_NAME}"
